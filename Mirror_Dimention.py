b2b5557fea9ac1c34981e9c3550320007f4ca485
praveen00143 <127049426+praveen00143@users.noreply.github.com>
2023-03-06 00:20:33 +0530

    Add files via upload

---
+++ Mirror_Dimention.py
@@ -0,0 +1,14 @@
+# Write a Python program that accepts a word from the user and reverse it.
+# Sample Test Case
+# Input : Edyoda
+# output: adoydE
+
+str1 = "Edyoda"
+
+for p in range(-1,-7,-1):
+    print(str1[p])
+
+output = []
+for p in range(-1,-7,-1):
+    output.append(str1[p])
+print(output)
